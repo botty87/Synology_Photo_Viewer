0535dc569e1f9fb574c467101236aafea3b3656d
botty <bottichio.marco@gmail.com>
2020-01-07 22:39:32 +0100

    working on 1.1-alpha2

---
+++ app/src/main/java/com/botty/photoviewer/data/fileStructure/MediaFile.kt
@@ -0,0 +1,27 @@
+package com.botty.photoviewer.data.fileStructure
+
+import io.objectbox.annotation.Entity
+import io.objectbox.annotation.Id
+import io.objectbox.annotation.Index
+import io.objectbox.relation.ToOne
+import java.io.File
+
+@Entity
+data class MediaFile(
+    @Id
+    var id: Long = 0,
+    val name: String,
+    @Index
+    val galleryId: Long,
+
+    @Transient
+    var file: File? = null,
+    @Transient
+    var timeoutException: Boolean = false
+) {
+    lateinit var folder: ToOne<MediaFolder>
+
+    constructor(id: Long, name: String, galleryId: Long ,folderId: Long) : this(id, name, galleryId) {
+        this.folder.targetId = folderId
+    }
+}
